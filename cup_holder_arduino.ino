0caf0df404d02287318601eda6ec7dadc78d76ed
Tim Jia <jtianbai@amazon.com>
2019-10-25 08:05:26 -0700

    Initial reading of weights

---
+++ cup_holder_arduino.ino
@@ -0,0 +1,21 @@
+/* Calibration sketch for HX711 */
+ 
+#include "HX711.h"  // Library needed to communicate with HX711 https://github.com/bogde/HX711
+ 
+#define DOUT  4  // Arduino pin 6 connect to HX711 DOUT
+#define CLK  5  //  Arduino pin 5 connect to HX711 CLK
+ 
+HX711 scale;  // Init of library
+
+void setup() {
+  Serial.begin(9600);
+  scale.begin(DOUT, CLK);
+  scale.set_scale();  // Start scale
+  scale.tare();       // Reset scale to zero
+}
+
+void loop() {
+  float current_weight=scale.get_units(20);  // get average of 20 scale readings
+  float scale_factor=(current_weight/0.145);  // divide the result by a known weight
+  Serial.println(scale_factor);  // Print the scale factor to use
+}
