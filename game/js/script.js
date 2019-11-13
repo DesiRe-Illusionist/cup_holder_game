884cd6c6027ece65543088feaf86937b7cb32e35
Tim Jia <jtianbai@amazon.com>
2019-11-13 07:33:04 -0800

    full animation, animation queue

--- game/js/script.js
+++ game/js/script.js
@@ -1,17 +1,13 @@
-fy = 0;
-my = 0;
-by = 0;
 dec = 300;
-speed = 800;
 effect = "linear";
 $(document).on("keydown", (e) => {
     if (e.which == 38) {
         //UP
-        if (fy + dec <= 7000) {
-            $("#front").transition({ y: fy + dec + "px" }, speed, effect)
-            $("#mid").transition({ y: (my + dec * 6 / 7) + "px" }, speed, effect)
-            $("#back").transition({ y: (by + dec * 5 / 7) + "px" }, speed, effect)
-            fy += dec;
+        if (flower_height + dec <= 7000) {
+            $("#front").transition({ y: flower_height + dec + "px" }, SPEED, effect)
+            $("#mid").transition({ y: (my + dec * 6 / 7) + "px" }, SPEED, effect)
+            $("#back").transition({ y: (by + dec * 5 / 7) + "px" }, SPEED, effect)
+            flower_height += dec;
             my += dec * 6 / 7;
             by += dec * 5 / 7;
         } 
@@ -19,30 +15,23 @@ $(document).on("keydown", (e) => {
         $("#water-text").addClass("run-drink")
         setTimeout(() => {
             $("#water-text").removeClass("run-drink")
-        }, speed);
+        }, TEXT_SPEED);
     } else if (e.which == 40) {
         //DOWN
-        if (fy - dec >= 0) {
-            $("#front").transition({ y: fy - dec + "px" }, speed, effect)
-            $("#mid").transition({ y: (my - dec * 6 / 7) + "px" }, speed, effect)
-            $("#back").transition({ y: (by - dec * 5 / 7) + "px" }, speed, effect)
-            fy -= dec;
+        if (flower_height - dec >= 0) {
+            $("#front").transition({ y: flower_height - dec + "px" }, SPEED, effect)
+            $("#mid").transition({ y: (my - dec * 6 / 7) + "px" }, SPEED, effect)
+            $("#back").transition({ y: (by - dec * 5 / 7) + "px" }, SPEED, effect)
+            flower_height -= dec;
             my -= dec * 6 / 7;
             by -= dec * 5 / 7;
         }
-    } else if (e.which == 39) {
-        /*
-         e.preventDefault();
-         $("#water-text").removeClass("run-drink")
-         void document.getElementById("#water-text").offsetWidth;
-         $("#water-text").addClass("run-drink")
-        */
     } else if (e.which == 37) {
         e.preventDefault();
         $("#water-text").addClass("run-drink")
         setTimeout(() => {
             $("#water-text").removeClass("run-drink")
-        }, speed);
+        }, TEXT_SPEED);
     }
     return false;
 })
--- game/js/visualization.js
+++ game/js/visualization.js
@@ -3,36 +3,82 @@ var rotateRand = 0.00;
 var xVelRand = 0.00;
 var yVelRand = 0.00;
 
+flower_height = 0;
+my = 0;
+by = 0;
+
+var pending_animation_queue = [];
+var currentState = {};
+var next = 0;
+
 const WEEKLY_WATER_VOLUME = 14000; // 2 liters a day
+const CURRENT_STATE = 'current_state';
+const SPEED = 500;
+const TEXT_SPEED = 800;
 
 function preload() {
-    previousState = readPreviousState();
-    latestState = pollLatestState();
+    currentState = readPreviousState();
+    pollLatestStateAndFindDiff();
 }
 
 function setup() {
-    const diff = compareStates(previousState.data, latestState.data);
-    const h = getHeightFromState(latestState.data);
-    setFlowerHeight(h);
-    console.log(h);
+    flower_height = getHeightFromState(currentState.data);
+    setFlowerHeight(flower_height);
 
 
     createCanvas(windowWidth, windowHeight);
-    img = loadImage('images/Cat2-4.png');
+    img = loadImage('assets/Cat2-4.png');
     imageMode(CENTER);
     blendMode(BLEND);
 }
 
 function draw() {
-    clear();
+    if (millis() > next) {
+        if (pending_animation_queue.length > 0) {
+            const drinking_activity = pending_animation_queue.shift();
+            growFlower(drinking_activity.volume);
+            console.log(drinking_activity);
 
+            currentState.data.push(drinking_activity);
+            localStorage.setItem(CURRENT_STATE, JSON.stringify(currentState));
+        }
+
+        next = millis() + 1000;
+        pollLatestStateAndFindDiff();
+    }
+
+
+    clear();
+    const flowerX = updateFlowerX();
+    const flowerY = updateFlowerY();
+    noFill();
+    stroke(2, 98, 0);
+    strokeWeight(13);
+    bezier(
+        windowWidth/2 - 10, windowHeight + flower_height, 
+        windowWidth/2 - 10, windowHeight + flower_height/2 , 
+        windowWidth/2, windowHeight, 
+        flowerX, flowerY
+    );
+    bezier(
+        windowWidth/2, windowHeight + flower_height, 
+        windowWidth/2, windowHeight + flower_height/2 , 
+        windowWidth/2, windowHeight, 
+        flowerX, flowerY
+    );
+    bezier(
+        windowWidth/2 + 10, windowHeight + flower_height, 
+        windowWidth/2 + 10, windowHeight + flower_height/2 , 
+        windowWidth/2, windowHeight, 
+        flowerX, flowerY
+    );
+
+    translate(flowerX, flowerY);
+
+    noStroke();
+    fill(251, 126, 126, 255);
     rotateRand = rotateRand + 0.005; // Change speed here
     var n = (noise(rotateRand) - 0.5);
-    
-    noStroke();
-    
-    translate(updateFlowerX(), updateFlowerY());
-    fill(251, 126, 126, 255);
     rotate(n);
     for (var i = 0; i < 10; i ++) {
         rotate(PI/5);
@@ -57,37 +103,36 @@ function updateFlowerY() {
     return yCenter;
 }
 
-function pollLatestState() {
-    return loadJSON('../data.json');
-}
-
 function readPreviousState() {
-    var prev_data = localStorage.getItem('prev_data');
+    var prev_data = localStorage.getItem(CURRENT_STATE);
 
     if (prev_data === null || prev_data === "") {
         var empty_data = '{ "data": [] }';
-        localStorage.setItem("prev_data", empty_data);
-        prev_data = localStorage.getItem('prev_data');
+        localStorage.setItem(CURRENT_STATE, empty_data);
+        prev_data = localStorage.getItem(CURRENT_STATE);
     }
 
     return JSON.parse(prev_data);
 }
 
-function compareStates(previousState, latestState) {
-    if (latestState.length < previousState.length) {
+function pollLatestStateAndFindDiff() {
+    return loadJSON('../data.json', pushLatestChangeToAnimationQueue);
+}
+
+async function pushLatestChangeToAnimationQueue(latestState) {
+    const registered_activity_length = currentState.data.length + pending_animation_queue.length;
+    if (latestState.data.length < registered_activity_length) {
         throw "Latest state length smaller than previous state. Corrupt data!";
     }
-    const diff = [];
-    for (var i = previousState.length; i < latestState.length; i++) {
-        diff.push(latestState[i]);
-    }
 
-    return diff;
+    for (var i = registered_activity_length; i < latestState.data.length; i++) {
+        pending_animation_queue.push(latestState.data[i]);
+    }
 }
 
 function getHeightFromState(state) {
     var volume = 0;
-    for (var i = 0; i < state.length; i++) {
+    for (var i = 0; i < currentState.data.length; i++) {
         volume += state[i].volume;
     }
     return getHeightFromVolume(volume);
@@ -98,16 +143,29 @@ function getHeightFromVolume(volume) {
 }
 
 function setFlowerHeight(height) {
-    document.getElementById('front').style.bottom = `calc(${-height}})`;
-    document.getElementById('mid').style.bottom = `calc(${-height})`;
-    document.getElementById('back').style.bottom = `calc(${-height})`;
+    flower_height = height;
+    my = height * 6 / 7;
+    by = height * 5 / 7;
+
+    document.getElementById('front').style.bottom = `calc(${-flower_height}px)`;
+    document.getElementById('mid').style.bottom = `calc(${-my}px)`;
+    document.getElementById('back').style.bottom = `calc(${-by}px)`;
 }
 
-function growFlower(height) {
-    $("#front").transition({ y: fy + height + "px" }, "linear")
-    $("#mid").transition({ y: (my + height * 6 / 7) + "px" }, "linear")
-    $("#back").transition({ y: (by + height * 5 / 7) + "px" }, "linear")
-    fy += dec;
-    my += dec * 6 / 7;
-    by += dec * 5 / 7;
+function growFlower(volume) {
+
+    const height = getHeightFromVolume(volume);
+
+    $("#front").transition({ y: flower_height + height + "px" }, SPEED, "linear")
+    $("#mid").transition({ y: (my + height * 6 / 7) + "px" }, SPEED, "linear")
+    $("#back").transition({ y: (by + height * 5 / 7) + "px" }, SPEED, "linear")
+    flower_height += height;
+    my += height * 6 / 7;
+    by += height * 5 / 7;
+
+    $("#water-text").html("+"+`${volume}`+"ml")
+    $("#water-text").addClass("run-drink")
+    setTimeout(() => {
+        $("#water-text").removeClass("run-drink")
+    }, TEXT_SPEED);
 }
