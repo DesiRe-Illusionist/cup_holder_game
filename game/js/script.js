55e60bd7281c6956369f28ba14399304b1dced07
Luke <ReventonC@users.noreply.github.com>
2019-11-13 10:16:21 -0500

    add drink text

--- game/js/script.js
+++ game/js/script.js
@@ -2,28 +2,47 @@ fy = 0;
 my = 0;
 by = 0;
 dec = 300;
+speed = 800;
+effect = "linear";
 $(document).on("keydown", (e) => {
     if (e.which == 38) {
         //UP
         if (fy + dec <= 7000) {
-            $("#front").transition({ y: fy + dec + "px" }, "linear")
-            $("#mid").transition({ y: (my + dec * 6 / 7) + "px" }, "linear")
-            $("#back").transition({ y: (by + dec * 5 / 7) + "px" }, "linear")
+            $("#front").transition({ y: fy + dec + "px" }, speed, effect)
+            $("#mid").transition({ y: (my + dec * 6 / 7) + "px" }, speed, effect)
+            $("#back").transition({ y: (by + dec * 5 / 7) + "px" }, speed, effect)
             fy += dec;
             my += dec * 6 / 7;
             by += dec * 5 / 7;
-        } else {
-        }
+        } 
+        e.preventDefault();
+        $("#water-text").addClass("run-drink")
+        setTimeout(() => {
+            $("#water-text").removeClass("run-drink")
+        }, speed);
     } else if (e.which == 40) {
         //DOWN
         if (fy - dec >= 0) {
-            $("#front").transition({ y: fy - dec + "px" }, "linear")
-            $("#mid").transition({ y: (my - dec * 6 / 7) + "px" }, "linear")
-            $("#back").transition({ y: (by - dec * 5 / 7) + "px" }, "linear")
+            $("#front").transition({ y: fy - dec + "px" }, speed, effect)
+            $("#mid").transition({ y: (my - dec * 6 / 7) + "px" }, speed, effect)
+            $("#back").transition({ y: (by - dec * 5 / 7) + "px" }, speed, effect)
             fy -= dec;
             my -= dec * 6 / 7;
             by -= dec * 5 / 7;
         }
+    } else if (e.which == 39) {
+        /*
+         e.preventDefault();
+         $("#water-text").removeClass("run-drink")
+         void document.getElementById("#water-text").offsetWidth;
+         $("#water-text").addClass("run-drink")
+        */
+    } else if (e.which == 37) {
+        e.preventDefault();
+        $("#water-text").addClass("run-drink")
+        setTimeout(() => {
+            $("#water-text").removeClass("run-drink")
+        }, speed);
     }
     return false;
 })
